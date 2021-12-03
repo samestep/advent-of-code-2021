d7d79f44439762b2c5c9737cc91ea812cef47d2f
Sam Estep <sam@samestep.com>
2021-12-03 08:41:12 -0500

    Fix output.json format

--- test.sh
+++ test.sh
@@ -9,8 +9,8 @@ diff --unified <(jq '.[0].input' output.json) \
   <(NO_COLOR=1 deno run --allow-read puzzle1.ts input.txt)
 
 echo 'puzzle 2 example'
-diff --unified <(jq '.[1][0].example' output.json) \
+diff --unified <(jq '.[1].example' output.json) \
   <(NO_COLOR=1 deno run --allow-read puzzle2.ts example.txt)
 echo 'puzzle 2 input'
-diff --unified <(jq '.[1][0].input' output.json) \
+diff --unified <(jq '.[1].input' output.json) \
   <(NO_COLOR=1 deno run --allow-read puzzle2.ts input.txt)
