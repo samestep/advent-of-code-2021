59c1312dac2720ab73724a3a30bda5db6a3537f5
Sam Estep <sam@samestep.com>
2021-12-26 08:07:49 -0500

    Make day 19 even faster

--- day19/shared.ts
+++ day19/shared.ts
@@ -83,12 +83,19 @@ export type Oriented = {
   beacons: Scanner;
 };
 
-const overlap = (s1: Scanner, s2: Scanner): Oriented | null => {
+const overlap = (
+  s1: Scanner,
+  s2: Scanner,
+  is1: Set<number>,
+  is2: Set<number>
+): Oriented | null => {
   const s2os = reorient(s2);
   for (let i = 0; i < s2os.length; ++i) {
     const s2o = s2os[i];
-    for (const p1 of s1) {
-      for (const p2 of s2o) {
+    for (const i1 of is1) {
+      const p1 = s1[i1];
+      for (const i2 of is2) {
+        const p2 = s2o[i2];
         const translation = zipWith(p1, p2, (a, b) => a - b);
         const beacons = s2o.map((p2) =>
           zipWith(p2, translation, (a, b) => a + b)
@@ -108,49 +115,56 @@ const overlap = (s1: Scanner, s2: Scanner): Oriented | null => {
 const euclidean = ([x1, y1, z1]: Point, [x2, y2, z2]: Point) =>
   (x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2;
 
-const intersect = <T>(
-  m1: Map<T, number>,
-  m2: Map<T, number>
-): Map<T, number> => {
-  const m = new Map<T, number>();
-  for (const [k, n1] of m1.entries()) {
-    const n2 = m2.get(k);
-    if (n2 !== undefined) {
-      m.set(k, Math.min(n1, n2));
+const intersect = <K, V>(
+  f: (v: V) => number,
+  m1: Map<K, V>,
+  m2: Map<K, V>
+): Map<K, number> => {
+  const m = new Map<K, number>();
+  for (const [k, v1] of m1.entries()) {
+    const v2 = m2.get(k);
+    if (v2 !== undefined) {
+      m.set(k, Math.min(f(v1), f(v2)));
     }
   }
   return m;
 };
 
+const select = <K, V1, V>(
+  f: (v: V1) => Set<number>,
+  m1: Map<K, V1>,
+  m: Map<K, V>
+): Set<number> => {
+  const s = new Set<number>();
+  for (const k of m.keys()) {
+    const v1 = m1.get(k);
+    if (v1 !== undefined) {
+      for (const i of f(v1)) {
+        s.add(i);
+      }
+    }
+  }
+  return s;
+};
+
 export const orientAll = (scanners: Scanner[]): Map<number, Oriented> => {
   const distances = scanners.map((scanner) => {
-    const m = new Map<number, number>();
+    const m = new Map<number, { n: number; s: Set<number> }>();
     for (let i = 0; i < scanner.length; ++i) {
       for (let j = i + 1; j < scanner.length; ++j) {
         const d = euclidean(scanner[i], scanner[j]);
-        const n = m.get(d);
-        m.set(d, n === undefined ? 1 : n + 1);
+        let v = m.get(d);
+        if (v === undefined) {
+          v = { n: 0, s: new Set() };
+          m.set(d, v);
+        }
+        ++v.n;
+        v.s.add(i);
+        v.s.add(j);
       }
     }
     return m;
   });
-  const candidates = new Map<number, number[]>();
-  for (let i = 0; i < scanners.length; ++i) {
-    for (let j = 0; j < scanners.length; ++j) {
-      if (i !== j) {
-        const m = intersect(distances[i], distances[j]);
-        const n = [...m.values()].reduce((a, b) => a + b, 0);
-        if (n >= 66) {
-          const a = candidates.get(i);
-          if (a === undefined) {
-            candidates.set(i, [j]);
-          } else {
-            a.push(j);
-          }
-        }
-      }
-    }
-  }
 
   const oriented = new Map<number, Oriented>([
     [0, { translation: [0, 0, 0], beacons: scanners[0] }],
@@ -158,11 +172,18 @@ export const orientAll = (scanners: Scanner[]): Map<number, Oriented> => {
   const todo = [0];
   while (todo.length > 0) {
     const i = todo.pop()!;
-    const js = candidates.get(i);
-    if (js !== undefined) {
-      for (const j of js) {
-        if (!oriented.has(j)) {
-          const ol = overlap(oriented.get(i)!.beacons, scanners[j]);
+    for (let j = 0; j < scanners.length; ++j) {
+      if (!oriented.has(j)) {
+        const m1 = distances[i];
+        const m2 = distances[j];
+        const m = intersect(({ n }) => n, m1, m2);
+        if ([...m.values()].reduce((a, b) => a + b, 0) >= 66) {
+          const ol = overlap(
+            oriented.get(i)!.beacons,
+            scanners[j],
+            select(({ s }) => s, m1, m),
+            select(({ s }) => s, m2, m)
+          );
           if (ol !== null) {
             oriented.set(j, ol);
             todo.push(j);
